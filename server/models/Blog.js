b19f0e5ed50d429ad0b90499588f57f94dcedce0
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-06-16 14:41:23 +0530

    Update Blog.js

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -7,6 +7,14 @@ const BlogSchema = new mongoose.Schema({
   imageData: Buffer, // Raw image binary
   imageContentType: String, // MIME type like "image/jpeg"
   createdAt: { type: Date, default: Date.now },
+  authorName: {
+    type: String,
+    default: 'Bharat'  
+  },
+  authorImage: {
+    type: String,
+    default: 'https://res.cloudinary.com/dok1a0vcc/image/upload/v1750056915/blog-images/qfapmrmhdxrekp6sg6l2.png'  
+  }
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
